06f3ce45d385f7d2a8ed27481ee09dfe0a19a68c
zhaochencheng <zhao.chencheng@utt.com.cn>
2018-01-19 21:16:39 +0800

    add utt RE-OS-3.0

---
+++ Re_OS_3/Config_data/config.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/1/19 16:25
+# @Author  : zhao.chencheng
+# @Email   : 907779487@qq.com
+# @File    : config.py
+# @Software: PyCharm Community Edition
+#登陆 url 用户名 密码
+url ="http://192.168.2.1"
+username = "admin"
+pwd = "admin"
+
+#外网配置 wan口ip 子网掩码 网关地址
+wan_ip = "192.168.30.115"
+wan_ip1 = "192.168.30.116"
+netmask = "255.255.255.0"
+GWaddr ="192.168.30.1"
+DNSaddr = "114.114.114.114"
