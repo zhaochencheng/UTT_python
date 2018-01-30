b95cdb2c97bad0549e319470e07a530e8b665796
zhaochencheng <zhao.chencheng@utt.com.cn>
2018-01-30 22:26:12 +0800

    add utt RE-OS-3.0 DDNS

--- Re_OS_3/Example/example.py
+++ Re_OS_3/Example/example.py
@@ -6,51 +6,47 @@
 # @Software: PyCharm Community Edition
 # from nose.tools import assert_equal
 # coding:utf-8
-#
+from Re_OS_3.Public.Login_Router import Login
 from selenium import webdriver
-from time import sleep
-import unittest,re
-# import Readexcel
-import os
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
-#excel表格获取url、用户名、密码
-# B=Readexcel.read_xls_file()
-url1="192.168.2.1"
-username="admin1"
-pwd="admin"
-
-def login_A():
-	print(url1)
-	driver.get("http://192.168.2.1")
-	driver.implicitly_wait(8)
-	driver.find_element_by_name("username").clear()
-	driver.find_element_by_name("username").send_keys(username)
-	driver.find_element_by_name("pwd").clear()
-	driver.find_element_by_name("pwd").send_keys(pwd)
-	driver.find_element_by_id("login_btn").click()
-	sleep(1)
-
-def check():
-#检测是否登录成功
-	for num in range(1,3):
-		if "index.html" in driver.current_url:
-			print('login successful')
-			#break #跳出当前循环
-			return #跳出检测函数
-		else:
-			error_mes=driver.find_element_by_xpath(".//*[@id='warning-msg']").text #获取错误信息
-			print (error_mes)
-			sleep(1)
-			login_A()
-#检测最后是否登录成功
-	if "index.html" in driver.current_url:
-		print('login successful')
-	else:
-		print("ERROR!")
-        raise BaseException("无法登陆页面！")
-if __name__ == '__main__':
+host = "http://192.168.32.115"
+port = ":8081"
+url = host+port
+# print(host+port)
+# driver = webdriver.Chrome()
+# Login(driver).login_router(url,"admin","admin")
+# print(driver.title)
+# print(driver.current_url)
+# driver.quit()
+import time
+try:
     driver = webdriver.Chrome()
+    driver.get(url)
     driver.implicitly_wait(10)
-    login_A()
-    check()
-    driver.quit()
+    webwait = WebDriverWait(driver, 10, 1)
+    # 用户名输入
+    # user = webwait.until(lambda x: x.find_element_by_name("username"))
+    # webwait.until(EC.visibility_of(By.NAME("username")))
+    for i in range(10):
+        if driver.find_element_by_name("username").is_displayed():
+            driver.find_element_by_name("username").clear()
+            driver.find_element_by_name("username").send_keys("admin")
+            # 密码输入
+            driver.find_element_by_name("pwd").clear()
+            driver.find_element_by_name("pwd").send_keys("admin")
+            # 点击登陆按钮
+            driver.find_element_by_id("login_btn").click()
+            break
+        else:
+            time.sleep(1)
+            print(i)
+        if i == 9:
+            raise BaseException("not open url")
+    # print("当前页面为：%s"%self.driver.title)
+    # time.sleep(3)
+except BaseException as  E:
+
+    print(E)
--- Re_OS_3/test_all/README.txt
+++ Re_OS_3/test_all/README.txt
@@ -37,10 +37,17 @@
 
     DHCP服务
 
-    端口映射 test_Port_mapping.py
+    端口映射 test_Port_mapping.py [未加判断该功能是否生效]
            ----静态映射配置,修改与删除    test_04_001_static_port_mapping[os对截图文件夹清空,再放截图][对配置的端口进行验证]
+           ----NAT规则 配置与删除        test_04_002_nat_rule
+           ----DMZ主机配置               test_04_003_DMZ [#添加DMZ 生效 方法][#单个wan口DMZ 配置]
+    路由配置 test_rouger_config.py
+            ----静态路由配置与删除        test_05_001_staticRouter
+            ----策略路由 开启 、关闭与策略路由配置   test_05_002_strategyRouter
 
-    动态域名
+
+    动态域名 test_DDNS.py
+           -----动态域名的新增与删除   test_006_ddns[判断 ddns功能是否生效；-----如果打不开怎么办]
 
     交换配置
 
