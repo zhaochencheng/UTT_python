cb64defd962b92e1a656403a5e9001771841167f
zhaochencheng <zhao.chencheng@utt.com.cn>
2018-02-04 16:53:10 +0800

    add utt RE-OS-3.0 test_accStatu.py add output_info allcontent

--- Re_OS_3/test_all/test_UserManage/test_accStatu.py
+++ Re_OS_3/test_all/test_UserManage/test_accStatu.py
@@ -28,21 +28,61 @@ class AccStatu(unittest.TestCase):
     def tearDown(self):
         time.sleep(5)
         self.driver.quit()
+    def Users_joingroups(self):
+        u'''将临时用户全部加入分组'''
+        #表头
+        all = self.driver.find_element_by_xpath("//*[@id='1']/div/div/div[1]/table/thead/tr/th[1]/input")
+        all.click()
+        # 移动到 定位
+        move = self.driver.find_element_by_id("move")
+        move.click()
+        #分组 定位
+        groupArr = self.driver.find_element_by_xpath(".//select[@name='groupArr']")
+        l = []
+        l= Select(groupArr).options
+        print("当前分组个数为：",len(l))
+        if len(l)==0:
+            print("当前无分组!无法移动到分组中！")
+            #关闭 定位
+            close = self.driver.find_element_by_id("modal-hide")
+            close.click()
+            raise BaseException("当前无分组!无法移动到分组中！")
+        else:
+            Select(groupArr).select_by_index(0)
+            time.sleep(2)
+            #保存 定位
+            save = self.driver.find_element_by_id("save")
+            save.click()
+            time.sleep(2)
+
+        # except BaseException as  E:
+        #     print("当前无分组!")
+
+
+
+
+
     def test_14_001_accStatu(self):
         u'''用户状态显示与操作'''
         # 显示等待
         webwait = WebDriverWait(self.driver, 10, 1)
         #用户管理 定位
-        usermanage = webwait.until(lambda x: x.find_element_xpath("//*[@id='sidebar']/ul/li[5]/div/h4/span"))
+        usermanage = webwait.until(lambda x: x.find_element_by_xpath("//*[@id='sidebar']/ul/li[5]/div/h4/span"))
         usermanage.click()
         #用户状态 定位
         accStatus = self.driver.find_element_by_link_text("用户状态")
         accStatus.click()
-        Output_info(self.driver).output_content()
+        #将页面 用户状态列表 信息输出
+        # Output_info(self.driver).output_all()
+        #将临时用户加入分组中
+        self.Users_joingroups()
 
+        #将页面 用户状态列表 信息输出
+        time.sleep(5)
+        self.driver.refresh()
+        time.sleep(3)
+        Output_info(self.driver).output_all()
 
-
-        pass
 
 
 
