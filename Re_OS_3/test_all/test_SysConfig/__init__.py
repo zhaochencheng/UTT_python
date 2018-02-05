71ebe7fb58c91a0e111f26aa5ee236ae20851703
zhaochencheng <907779487@qq.com>
2018-02-05 23:16:45 +0800

    add REos system_admin

---
+++ Re_OS_3/test_all/test_SysConfig/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/2/5 21:52
+# @Author  : zhao.chencheng
+# @Email   : 907779487@qq.com
+# @File    : __init__.py.py
+# @Software: PyCharm
---
+++ Re_OS_3/test_all/test_SysConfig/test_NetManageStrate.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/2/5 21:55
+# @Author  : zhao.chencheng
+# @Email   : 907779487@qq.com
+# @File    : test_NetManageStrate.py
+# @Software: PyCharm
