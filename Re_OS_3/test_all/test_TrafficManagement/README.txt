92874d48934107b8ab1ba638a90443b933be6d25
zhaochencheng <zhao.chencheng@utt.com.cn>
2018-03-06 11:30:53 +0800

    3.0 代码重构1 Application priority

--- Re_OS_3/test_all/test_TrafficManagement/README.txt
+++ Re_OS_3/test_all/test_TrafficManagement/README.txt
@@ -12,5 +12,17 @@
 *****************************************
 -----------------------------------------
 流量管理 test_TrafficManagement
-        -----应用优先   test_Application_priority.py
+        -----应用优先    test_Application_priority.py
+                应用管理--开启                test_21_001_Application_priority_open
+                办公优先配置                  test_21_002_Application_priority_Office_priority_config
+                办公优先配置页面内容输出       test_21_003_Application_priority_Office_priority_show
+                办公优先配置 功能生效          test_21_004_Application_priority_Office_priority_validate (未实现)
+                办公优先配置删除               test_21_005_Application_priority_Office_priority_delect
+
+                娱乐优先配置                   test_21_007_Application_priority_Entertainment_priority_config
+                娱乐优先配置页面内容输出       test_21_008_Application_priority_Entertainment_priority_show
+                娱乐优先配置 功能生效          test_21_009_Application_priority_Entertainment_priority_validate(未实现)
+                娱乐优先配置删除               test_21_010_Application_priority_Entertainment_priority_delect
+                应用管理--关闭                 test_21_011_Application_priority_close
+
         -----流量管理
--- Re_OS_3/test_all/test_TrafficManagement/test_Application_priority.py
+++ Re_OS_3/test_all/test_TrafficManagement/test_Application_priority.py
@@ -26,33 +26,6 @@ class Application_priority(unittest.TestCase):
     def tearDown(self):
         time.sleep(5)
         self.driver.quit()
-    def check_imgopen(self,location):
-        u'''检查应用优先总开关 是否开启'''
-        if location.get_attribute("checktype") == "0" :
-            print(location.get_attribute("checktype"))
-            print("应用优先总开关 -- 未开启！")
-            print("现在开启")
-            location.click()
-            time.sleep(3)
-        else:
-            print("应用优先总开关 -- 已开启！")
-
-    def select_apptype(self,location):
-        l = Select(location).options
-        for i in range(len(l)):
-            name = l[i].text
-            print("%d:" % i, name)
-            if name == '自定义':
-                pass
-            else:
-                Select(location).select_by_index(i)
-                # 确定 定位
-                changeAppType = self.driver.find_element_by_id("changeAppType")
-                changeAppType.click()
-                time.sleep(2)
-                #输出配置的 模板样式
-                # Output_info(self.driver).output_all()
-                time.sleep(2)
 
     def switch_check_open(self,state):
         u"0代表关闭,1代表开启"
@@ -83,52 +56,6 @@ class Application_priority(unittest.TestCase):
         app_priority.click()
         print("当前位置:", app_priority.text)
 
-
-
-
-    def test_21_001_Application_priority(self):
-        u'''应用管理开启-操作-关闭'''
-        #
-        self.enter_app_priority()
-
-        #检查应用优先总开关 是否开启
-        img = self.driver.find_element_by_id("checkOpen")
-        self.check_imgopen(img)
-        time.sleep(2)
-
-        #选择应用场景:
-        apptype = self.driver.find_element_by_xpath(".//select[@name='appType']")
-        # self.select_apptype(apptype)
-        l = Select(apptype).options
-        # print(len(l))
-        for i in range(len(l)):
-            # print(l[i])
-            name = l[i].text
-            print("%d:" % i, name)
-            Select(apptype).select_by_index(i)
-            time.sleep(3)
-            if name == '自定义':
-                pass
-            else:
-                # apptype = self.driver.find_element_by_xpath(".//select[@name='appType']")
-                # Select(apptype).select_by_index(i)
-                # 确定 定位
-                changeAppType = self.driver.find_element_by_id("changeAppType")
-                changeAppType.click()
-                time.sleep(2)
-                # 输出配置的 模板样式
-                # Output_info(self.driver).output_all()
-                time.sleep(2)
-            # if name == '娱乐优先':
-            #     Select(apptype).select_by_index(i)
-            #     # 确定 定位
-            #     changeAppType = self.driver.find_element_by_id("changeAppType")
-            #     changeAppType.click()
-            #     time.sleep(2)
-            #     # 输出配置的 模板样式
-            #     # Output_info(self.driver).output_all()
-            #     time.sleep(2)
-
     def test_21_001_Application_priority_open(self):
         u"应用管理--开启"
         #进入流量管理---应用优先页面
@@ -152,16 +79,21 @@ class Application_priority(unittest.TestCase):
         # 确定 定位
         changeAppType = self.driver.find_element_by_id("changeAppType")
         changeAppType.click()
-    def test_21_002_Application_priority_Office_priority_show(self):
+        time.sleep(2)
+        '''判断是否生成了办公优先模板'''
+        rulename = self.driver.find_element_by_xpath("//*[@id='1']/div/div[1]/div[1]/table/tbody/tr[1]/td[3]/span")
+        self.assertIn("办公优先", rulename.text, "未生成了办公优先模板")
+        print("办公优先模板--现在生成")
+    def test_21_003_Application_priority_Office_priority_show(self):
         u'''办公优先配置页面内容输出'''
         # 进入流量管理---应用优先页面
         self.enter_app_priority()
         #输出页面内容
         Output_info(self.driver).output_all()
-    def test_21_002_Application_priority_Office_priority_validate(self):
+    def test_21_004_Application_priority_Office_priority_validate(self):
         u'''办公优先配置 功能生效'''
         pass
-    def test_21_002_Application_priority_Office_priority_delect(self):
+    def test_21_005_Application_priority_Office_priority_delect(self):
         u'''办公优先配置删除'''
         # 进入流量管理---应用优先页面
         self.enter_app_priority()
@@ -169,23 +101,50 @@ class Application_priority(unittest.TestCase):
         Delect_config(self.driver).delect_all_config()
 
 
+    def test_21_007_Application_priority_Entertainment_priority_config(self):
+        u"娱乐优先配置"
+        #进入流量管理--应用优先页面
+        self.enter_app_priority()
+        # 选择娱乐优先
+        # 选择应用场景:
+        apptype = self.driver.find_element_by_xpath(".//select[@name='appType']")
+        Select(apptype).select_by_visible_text("娱乐优先")
+        # 确定 定位
+        changeAppType = self.driver.find_element_by_id("changeAppType")
+        changeAppType.click()
+        time.sleep(2)
+        '''判断是否生成了娱乐优先模板'''
+        rulename = self.driver.find_element_by_xpath("//*[@id='1']/div/div[1]/div[1]/table/tbody/tr[1]/td[3]/span")
+        self.assertIn("娱乐优先",rulename.text,"未生成了娱乐优先模板")
+        print("娱乐优先模板--现在生成")
+    def test_21_008_Application_priority_Entertainment_priority_show(self):
+        u'''娱乐优先配置页面内容输出'''
+        # 进入流量管理---应用优先页面
+        self.enter_app_priority()
+        # 输出页面内容
+        Output_info(self.driver).output_all()
+    def test_21_009_Application_priority_Entertainment_priority_validate(self):
+        u'''娱乐优先配置 功能生效'''
+        pass
+    def test_21_010_Application_priority_Entertainment_priority_delect(self):
+        u'''娱乐优先配置删除'''
+        # 进入流量管理---应用优先页面
+        self.enter_app_priority()
+        # 删除 娱乐优先配置
+        Delect_config(self.driver).delect_all_config()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    def test_21_011_Application_priority_close(self):
+        u"应用管理--关闭"
+        #进入流量管理---应用优先页面
+        self.enter_app_priority()
+        #开启应用优先按钮
+        #0代表关闭,1代表开启
+        self.switch_check_open(0)
+        '''判断按钮是否开启'''
+        checkopen = self.driver.find_element_by_id("checkOpen")
+        checktype = checkopen.get_attribute("checktype")
+        self.assertEqual(int(checktype),0,"按钮当前状态为开启状态；关闭功能--失败！！")
+        print("现在已关闭--应用优先 功能")
 
 
 if __name__ == '__main__':
